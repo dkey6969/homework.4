e6cad007fabbc2e29f62e2a36422f07445f94272
dkey6969 <heromamasky@>
2024-11-07 13:55:31 +0600

    add random recipe

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bot_config import bot, dp
 from handlers.start import start_router
 from handlers.picture import picture_router
 from handlers.other_messages import echo_router
+from handlers.random_recipe import random_recipe_router
 from handlers.minfo import minfo_router
 
 
@@ -12,6 +13,7 @@ async def main():
     dp.include_router(start_router)
     dp.include_router(picture_router)
     dp.include_router(minfo_router)
+    dp.include_router(random_recipe_router)
     dp.include_router(echo_router)
 
     # запуск бота:
