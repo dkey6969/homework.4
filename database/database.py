cf45af9866f5327e277f42fe5a28ddce6a379926
dkey6969 <heromamasky@>
2024-11-18 21:51:02 +0600

    added homework

--- database/database.py
+++ database/database.py
@@ -1,34 +1,22 @@
 import sqlite3
 
 class Database:
-    def __init__(self, db_name="bot_database.db"):
-        self.db_name = db_name
-        self.connection = sqlite3.connect(self.db_name)
-        self.cursor = self.connection.cursor()
+    def __init__(self, path: str):
+        self.path = path
 
     def create_tables(self):
-        self.cursor.execute("""
-        CREATE TABLE IF NOT EXISTS reviews (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            phone_or_instagram TEXT,
-            visit_date TEXT,
-            food_rating INTEGER,
-            cleanliness_rating INTEGER,
-            extra_comments TEXT
-        )
-        """)
-        self.connection.commit()
-
-    def execute(self, query, params):
-        self.cursor.execute(query, params)
-        self.connection.commit()
-
-    def fetchall(self, query, params):
-        self.cursor.execute(query, params)
-        return self.cursor.fetchall()
-
-    def close(self):
-        self.connection.close()
-
-
+        with sqlite3.connect(self.path) as conn:
+            conn.execute(
+                """
+                    CREATE TABLE IF NOT EXISTS survey_results (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT,
+                        phone_or_instagram INTEGER,
+                        visit_date TEXT,
+                        food_rating TEXT,
+                        cleanliness_rating TEXT,
+                        extra_comments_rating TEXT
+                    )
+                """
+            )
+            conn.commit()
